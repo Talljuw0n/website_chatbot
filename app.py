7a60b5618beb6e64c9e6adf24bda315c54e61c29
Talljuw0n <asujuwon3@gmail.com>
2025-05-24 19:31:00 +0100

    Updated Commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 from scraper import scrape_website
 import requests
 import os
